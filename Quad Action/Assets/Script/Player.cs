ee74c9524711b0bd2f0c412bd205ea61bfb9754e
jooyoung cha <ryuha.kuma@gmail.com>
2022-08-06 18:04:20 +0900

    #2 22.08.06 2차시
    플레이어 회피/점프
    static을 적용시킬 경우 Mesh cube 관련 오류가 발생함 해결하는 방법 학습필요

--- Quad Action/Assets/Script/Player.cs
+++ Quad Action/Assets/Script/Player.cs
@@ -10,13 +10,19 @@ public class Player : MonoBehaviour
     float hAxis;        //횡이동
     float vAxis;        //종이동
     bool wDown;         //walk 조작키입력(shift)
+    bool jDown;         //점프
+    bool isjump;
+    bool isDodge;
+
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
     Animator anim;
 
-
+    Rigidbody rigid;
     private void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
         first_speed = speed;
     }
@@ -32,10 +38,11 @@ public class Player : MonoBehaviour
         getInput();
         move();
         turn();
+        jump();
+        dodge();
+
+
         
-
-
-       
     }
 
     void getInput()
@@ -43,6 +50,7 @@ public class Player : MonoBehaviour
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");            //Shift 누르고 있을때만 작동
+        jDown = Input.GetButtonDown("Jump");
     }
 
     void move()
@@ -60,6 +68,9 @@ public class Player : MonoBehaviour
         }
         transform.position += moveVec * speed * Time.deltaTime;*/
         //삼항연산자 써서 한줄로 작성   wDown이 true이면 1.0, false면 first_speed
+        if (isDodge)
+            moveVec = dodgeVec;
+
         transform.position += moveVec * (wDown ? 1.0f : first_speed) * Time.deltaTime;
 
 
@@ -72,5 +83,52 @@ public class Player : MonoBehaviour
         //LookAt() 지정된 벡터를 향해서 회전시켜주는 함수
         //현재 위치 + 나아가야하는 방향벡터로 회전
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void jump()
+    {
+        if (jDown && moveVec == Vector3.zero && !isjump && !isDodge)
+        {
+            rigid.AddForce(Vector3.up * 15 ,ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump"); 
+            isjump = true;
+        }
+        
+    }
+
+    void dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isjump )
+        {
+
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+
+            //시간차 호출
+            Invoke("dodgeOut", 0.5f);
+        }
+
+    }
+
+    void dodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "floor")
+        {
+            isjump = false;
+            anim.SetBool("isJump", false);
+
+        }
     }
 }
