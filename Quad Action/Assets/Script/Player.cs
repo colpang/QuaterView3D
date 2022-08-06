a85417fff27960051051a13db245f0323f0e950f
jooyoung cha <ryuha.kuma@gmail.com>
2022-08-06 17:11:15 +0900

    코드 정리

--- Quad Action/Assets/Script/Player.cs
+++ Quad Action/Assets/Script/Player.cs
@@ -29,9 +29,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        getInput();
+        move();
+        turn();
+        
+
+
+       
+    }
+
+    void getInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");            //Shift 누르고 있을때만 작동
+    }
+
+    void move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
 
@@ -48,10 +63,12 @@ public class Player : MonoBehaviour
         transform.position += moveVec * (wDown ? 1.0f : first_speed) * Time.deltaTime;
 
 
-        anim.SetBool("isRun",moveVec != Vector3.zero);
+        anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
+    }
 
-
+    void turn()
+    {
         //LookAt() 지정된 벡터를 향해서 회전시켜주는 함수
         //현재 위치 + 나아가야하는 방향벡터로 회전
         transform.LookAt(transform.position + moveVec);
